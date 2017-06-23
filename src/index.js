0032590e9c00f610b7195fe7b2ef782162571c86
Rebekah Cheng <work@rebekahcheng.com>
2017-06-23 11:15:59 +0100

    installed Babel plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+// importing React which is the library that allows us to build React components
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// importing ReactDOM which is the library that allows us to place our components and work with them in the context of the DOM
+import ReactDOM from 'react-dom';
+
+// importing the component that we just worked on, the App component
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+  );
+
